a69cc4ddc38483aa5828d7986cbc1f4b679ab6f9
360NENZ | AstralTz <gdgwhy@outlook.com>
2025-07-17 16:17:21 +0800

    fix: videobg mismatch chore: add MPL 2.0 license chore: rm useless files feat: use own umami

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ var api = "https://blog.360nenz.top/";
 
 $(document).ready(function () {
     $(".loading").hide();
-    getAchives();
+    getAchieves();
     getHitokoto();
 });
 
@@ -18,7 +18,7 @@ function switchTo(target) {
     $(target).addClass('active');
 }
 
-function getAchives() {
+function getAchieves() {
     t = ``;
     $.ajax({
         type: "GET",
@@ -38,7 +38,7 @@ function getAchives() {
 
 function getHitokoto() {
     $.ajax({
-        url: "https://api.360nenz.top/api/hitokoto",
+        url: "https://api.tomys.top/api/hitokoto?c=a",
         dataType: "json",
         success: function (result) {
             write(result.hitokoto + " —— " + result.from);
@@ -50,7 +50,7 @@ function getHitokoto() {
 }
 
 function write(text) {
-    if (text.length < 30) {
+    if (text.length <= 50) {
         $('#hitokoto').html(text);
     } else {
         getHitokoto();
@@ -58,48 +58,23 @@ function write(text) {
 }
 
 // 对Date的扩展，将 Date 转化为指定格式的String
-// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
-// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
-// 例子： 
-// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 
-// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18 
-Date.prototype.Format = function (fmt) { //author: meizz 
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+// 例子：
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
+Date.prototype.Format = function (fmt) { //author: meizz
     var o = {
-        "M+": this.getMonth() + 1, //月份 
-        "d+": this.getDate(), //日 
-        "h+": this.getHours(), //小时 
-        "m+": this.getMinutes(), //分 
-        "s+": this.getSeconds(), //秒 
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
     };
     if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
     for (var k in o)
         if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
-
-//异步加载背景
-
-function blobToDataURI(blob, callback) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        callback(e.target.result);
-    }
-    reader.readAsDataURL(blob);
-}
-var url = "https://api.tomys.top/api/acgimg";
-var xhr = new XMLHttpRequest();
-xhr.open('GET', url, true);
-xhr.responseType = "blob";
-xhr.onload = function () {
-    if (this.status == 200) {
-        var blob = this.response;
-        blobToDataURI(blob, function (t) {
-            $("body").css("background-image", "url('" + t + "')");
-            $("#background-small").addClass("smallBg");
-            $("#background-small").css("opacity", "0");
-        });
-    }
-}
-xhr.send();
